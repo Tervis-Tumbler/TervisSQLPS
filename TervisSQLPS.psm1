f56ec03b0aaae904ef36ec30119e0c0df20d8cbd
Chris Magnuson <hourglasssand@gmail.com>
2017-01-28 08:26:56 -0500

    Add Remove-DataCollectionSetCacheFiles

---
+++ TervisSQLPS.psm1
@@ -0,0 +1,9 @@
+﻿function Remove-DataCollectionSetCacheFiles {
+    param (
+        $ComputerName
+    )
+    Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+        $CacheFiles = Get-ChildItem -Path $env:windir\Temp -File -Filter *.cache
+        $CacheFiles | Remove-Item
+    }
+}
