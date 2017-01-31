af5458a7f954e4fc4bf16b35d06fd33548dbd717
Chris Magnuson <hourglasssand@gmail.com>
2017-01-31 12:13:54 -0500

    Added functions used when troubleshooting RMS issues

--- TervisSQLPS.psm1
+++ TervisSQLPS.psm1
@@ -7,3 +7,78 @@
         $CacheFiles | Remove-Item
     }
 }
+
+function Get-SQLBackupJobStatus {
+    param (
+        $ComputerName
+    )
+    Invoke-SQL -dataSource $ComputerName -database Master -sqlCommand @"
+SELECT session_id as SPID, command, a.text AS Query, start_time, percent_complete, dateadd(second,estimated_completion_time/1000, getdate()) as estimated_completion_time
+FROM sys.dm_exec_requests r CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) a
+WHERE r.command in ('BACKUP DATABASE','RESTORE DATABASE','BACKUP LOG')
+"@
+}
+
+function Get-LatestOlaHalengrenMaintenanceActivity {
+    param (
+        $ComputerName
+    )
+    Invoke-SQL -dataSource $ComputerName -database Master -sqlCommand @"
+SELECT TOP 10 [ID]
+      ,[StartTime]
+      ,[EndTime]
+      ,[DatabaseName]
+      ,[SchemaName]
+      ,[ObjectName]
+      ,[ObjectType]
+      ,[IndexName]
+      ,[IndexType]
+      ,[ExtendedInfo]
+      ,[Command]
+      ,[CommandType]
+      ,[ErrorNumber]
+      ,[ErrorMessage]
+  FROM [master].[dbo].[CommandLog]
+order by ID desc
+"@
+
+}
+
+function Get-GhostRecordCounts {
+<#
+.EXAMPLE
+
+Get-GhostRecordCounts -ComputerName SQLServerName -DatabaseName DatabaseName -TableName dbo.Table
+#>
+    param (
+        $ComputerName,
+        $DatabaseName,
+        $TableName
+    )
+    Invoke-SQL -dataSource $ComputerName -database $DatabaseName -sqlCommand @"
+SELECT * FROM sys.dm_db_index_physical_stats
+(DB_ID(N'$DatabaseName'), OBJECT_ID(N'$TableName'), NULL, NULL , 'DETAILED')
+"@
+}
+
+function Get-SQLObjectName {
+    param (
+        $ComputerName,
+        $DatabaseID,
+        $ObjectID
+    )
+    Invoke-SQL -dataSource $ComputerName -database Master -sqlCommand @"
+select OBJECT_NAME ($ObjectID, $DatabaseID)
+"@
+}
+
+function Get-SQLIndexName {
+    param (
+        $ComputerName,
+        $IndexID,
+        $ObjectID
+    )
+    Invoke-SQL -dataSource $ComputerName -database $DatabaseName -sqlCommand @"
+SELECT name FROM sys.indexes WHERE object_id = $ObjectID and index_id = $IndexID
+"@
+}
