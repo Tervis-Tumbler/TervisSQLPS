0d34dc4dee550118970cc0d0d3a72ba03ee866b1
Chris Magnuson <hourglasssand@gmail.com>
2017-03-31 15:56:44 -0400

    Additional functions

--- TervisSQLPS.psm1
+++ TervisSQLPS.psm1
@@ -53,23 +53,24 @@ Get-GhostRecordCounts -ComputerName SQLServerName -DatabaseName DatabaseName -Ta
     param (
         $ComputerName,
         $DatabaseName,
-        $TableName
+        $TableName,
+        $SchemaName = "dbo"
     )
-    Invoke-SQL -dataSource $ComputerName -database $DatabaseName -sqlCommand @"
+    Invoke-SQL -dataSource $ComputerName -database $DatabaseName -ConvertFromDataRow -sqlCommand @"
 SELECT * FROM sys.dm_db_index_physical_stats
-(DB_ID(N'$DatabaseName'), OBJECT_ID(N'$TableName'), NULL, NULL , 'DETAILED')
-"@
+(DB_ID(N'$DatabaseName'), OBJECT_ID(N'$DatabaseName.$SchemaName.$TableName'), NULL, NULL , 'DETAILED')
+"@ | where ghost_record_count
 }
 
 function Get-SQLTableNameFromObjectID {
     param (
         $ComputerName,
-        $DatabaseID,
-        $TableObjectID
+        [int]$DatabaseID,
+        [int]$TableObjectID
     )
-    Invoke-SQL -dataSource $ComputerName -database Master -sqlCommand @"
-select OBJECT_NAME ($ObjectID, $DatabaseID)
-"@
+    Invoke-SQL -dataSource $ComputerName -database Master -ConvertFromDataRow -sqlCommand @"
+select OBJECT_NAME ($TableObjectID, $DatabaseID)
+"@ | select -ExpandProperty Column1
 }
 
 function Get-SQLIndexNameFromID {
@@ -106,4 +107,33 @@ function Get-ConnectionsToSpecificDatabase {
     )
     Get-SQLSPWho2 -ComputerName $ComputerName | 
     where DBName -EQ $DatabaseName
+}
+
+function Get-DatabaseTableNames {
+    param (
+        $ComputerName,
+        $DatabaseName
+    )
+   
+    Invoke-SQL -dataSource $ComputerName -database $DatabaseName -ConvertFromDataRow -sqlCommand @"
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='$DatabaseName'
+"@ | select -ExpandProperty Table_Name
+}
+
+function Invoke-FullScanOfAllTablesInDatabase {
+    [cmdletbinding()]
+    param (
+        $ComputerName,
+        $DatabaseName
+    )
+    foreach ($TableName in $(Get-DatabaseTableNames @PSBoundParameters | Sort)) {
+        Write-Verbose "Starting $TableName"
+        Invoke-SQL -dataSource $ComputerName -database $DatabaseName -sqlCommand @"
+SELECT * FROM $TableName with (nolock)
+"@ | Out-Null
+        Write-Verbose "Finished $TableName"
+    }
+
 }
